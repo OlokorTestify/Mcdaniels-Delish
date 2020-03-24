e2fda8092fd7a8b7bb2434be225c8ca9f1265543
olokor testify <olokortestify1999@gmail.com>
2020-03-24 20:10:37 +0100

    worked on what we do section

---
+++ src/containers/Landing/components/What/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './index.scss'
+
+const What  = () => (
+    <>
+        <section className='what'>
+            <div className="what-main">
+                <h1>What we do?..</h1>
+                <p>We offer a variety of local Nigerian delicacies on our menu list. All hygienically prepared and well packaged.We are very devoted to giving personal attention to every details of our customer’s requirement, no matter how small. We offer impeccable bespoke cooking; Tailored to Your taste and preference, hygienically prepared, properly sealed and delivered conveniently to your doorstep,including office delivery.</p>
+            </div>
+        </section>
+    </>
+)
+
+export default What;
--- src/containers/Landing/index.jsx
+++ src/containers/Landing/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Header from './components/Header'
+import What from './components/What'
 import Why from './components/Why'
 
 
@@ -10,6 +11,7 @@ const Landing = () => {
     return (
         <>
             <Header />
+            <What />
             <Why />
         </>
     )
